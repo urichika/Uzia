a4f8c195eeb2adbacf2aef9fffa3a7594f12179d
uri <uri@git.com>
2021-11-17 19:00:21 +0900

    Add Heroku configuration

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const mysql = require('mysql');
-
-const app = express();
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
 app.use(express.static('public')); //ワンちゃんこの文いらない
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: 'us-cdbr-east-04.cleardb.com',
   user: 'b431019deb93b8',
   password: '3ecab8a6',
@@ -26,100 +25,121 @@ connection.connect((err) => {
 
 
   app.get('/', (req, res) => {
-    connection.query(
-      'SELECT \
-         id,ブルワリー,name,IBU,ABV,スタイル \
-       FROM \
-         beers',
-        (error, results) => {
-            res.render('top.ejs', {beers: results});
-        }
-    )
+    pool.getConnection(function(err, connection){
+      connection.query(
+        'SELECT \
+           id,ブルワリー,name,IBU,ABV,スタイル \
+         FROM \
+           beers',
+          (error, results) => {
+              res.render('top.ejs', {beers: results});
+              connection.release();
+          }
+      );
+    });
   });
 
   app.get('/Food',(req,res) => {
-    connection.query(
-      'SELECT \
-         name,price,genre,info \
-       FROM \
-         foods \
-       ORDER BY \
-         genre asc,name asc,id asc',
-        (error, results) => {
-            res.render('Food.ejs', {foods: results});
-        }
-    )
-  })
+    pool.getConnection(function(err, connection){
+      connection.query(
+        'SELECT \
+           name,price,genre,info \
+         FROM \
+           foods \
+         ORDER BY \
+           genre asc,name asc,id asc',
+          (error, results) => {
+              res.render('Food.ejs', {foods: results});
+              connection.release();
+          }
+      );
+    });
+  });
 
   app.get('/Drink',(req,res) => {
-    connection.query(
-      'SELECT \
-         name,price,genre,base1 \
-       FROM \
-         drinks \
-       ORDER BY \
-         genre asc,base1 asc,name asc',
-        (error, results) => {
-            res.render('Drink.ejs', {drinks: results});
-        }
-    )
-  })
+    pool.getConnection(function(err, connection){
+      connection.query(
+        'SELECT \
+           name,price,genre,base1 \
+         FROM \
+           drinks \
+         ORDER BY \
+           genre asc,base1 asc,name asc',
+          (error, results) => {
+              res.render('Drink.ejs', {drinks: results});
+              connection.release();
+          }
+      );
+    });
+  });
 
   app.get('/Limited',(req,res) => {
-    connection.query(
-      'SELECT \
-         name,price,genre,info \
-       FROM \
-         limiteds \
-       ORDER BY \
-         genre asc,name asc,id asc',
-        (error, results) => {
-            res.render('Limited.ejs', {limiteds: results});
-        }
-    )
-  })
+    pool.getConnection(function(err, connection){
+      connection.query(
+        'SELECT \
+           name,price,genre,info \
+         FROM \
+           limiteds \
+         ORDER BY \
+           genre asc,name asc,id asc',
+          (error, results) => {
+              res.render('Limited.ejs', {limiteds: results});
+              connection.release();
+          }
+      );
+    });
+  });
 
   app.get('/Drink/BaseSearch',(req,res) => {
-    const Base = req.query.Base;
-    connection.query(
-      'SELECT \
-         name,genre,base1,base2,base3,base4,base5,base6,base7 \
-       FROM \
-         drinks \
-       WHERE \
-         genre = "カクテル" \
-         AND (base1 = ? \
-         OR base2 = ? \
-         OR base3 = ? \
-         OR base4 = ? \
-         OR base5 = ?)',
-      //LIKEの後ろにワイルドカード(%)が使えないけど妥協
-      [Base,Base,Base,Base,Base],
-        (error, results) => {
-            res.render('BaseSearch.ejs', {kakuterus: results});
-        }
-    )
-  })
+    pool.getConnection(function(err, connection){
+    var Base = req.query.Base;
+      connection.query(
+        'SELECT \
+           name,genre,base1,base2,base3,base4,base5,base6,base7 \
+         FROM \
+           drinks \
+         WHERE \
+           genre = "カクテル" \
+           AND (base1 = ? \
+           OR base2 = ? \
+           OR base3 = ? \
+           OR base4 = ? \
+           OR base5 = ?)',
+        //LIKEの後ろにワイルドカード(%)が使えないけど妥協
+        [Base,Base,Base,Base,Base],
+          (error, results) => {
+              res.render('BaseSearch.ejs', {kakuterus: results});
+              connection.release();
+          }
+      );
+    });
+  });
 
   app.get('/Drink/NameSearch',(req,res) => {
-      const Name = req.query.Name;
-    connection.query(
-      'SELECT \
-         name,genre,base1,base2,base3,base4,base5,base6,base7 \
-       FROM \
-         drinks \
-       WHERE \
-         genre = "カクテル" \
-         AND name = ?',
-      [Name],
-        (error, results) => {
-            res.render('NameSearch.ejs', {kakuterus: results});
-        }
-    )
-})
+    pool.getConnection(function(err, connection){
+    var Name = req.query.Name;
+      connection.query(
+        'SELECT \
+           name,genre,base1,base2,base3,base4,base5,base6,base7 \
+         FROM \
+           drinks \
+         WHERE \
+           genre = "カクテル" \
+           AND name = ?',
+        [Name],
+          (error, results) => {
+              res.render('NameSearch.ejs', {kakuterus: results});
+              connection.release();
+          }
+      );
+    });
+  });
 
-app.get('/access', (req, res) => {
-    res.render('access.ejs');
-});
+  app.get('/access', (req, res) => {
+    pool.getConnection(function(err, connection){
+      res.render('access.ejs');
+      connection.release();
+    });
+  });
 
 app.listen(process.env.PORT || 5000)
